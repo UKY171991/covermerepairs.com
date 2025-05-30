c9c607db6a5291005a9499dd5e789fa051f87981
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-05-30 09:59:05 +0530

    Update Stock_in.php

--- application/controllers/Stock_in.php
+++ application/controllers/Stock_in.php
@@ -3,8 +3,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Stock_in extends CI_Controller {
     public function index() {
-        $this->load->view('inc/header');
-        $this->load->view('stock_in');
-        $this->load->view('inc/footer');
+        $data['ajax'] = 'none';
+        $this->load->view('inc/header', $data);
+        $this->load->view('stock_in', $data);
+        $this->load->view('inc/footer', $data);
     }
 } 
