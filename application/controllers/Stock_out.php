423f6ada70308c5b4ee8d6597488fdd0a80193ad
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-05-30 10:24:23 +0530

    Update Stock_out.php

--- application/controllers/Stock_out.php
+++ application/controllers/Stock_out.php
@@ -10,4 +10,24 @@ class Stock_out extends CI_Controller {
         $this->load->view('stock_out', $data);
         $this->load->view('inc/footer', $data);
     }
+
+    public function add_stock_out() {
+        $this->load->model('Stock_model', 'stock');
+        $data = $this->input->post();
+        $id = $this->stock->insert_stock_out($data);
+        echo json_encode(['status' => 'success', 'id' => $id]);
+    }
+
+    public function edit_stock_out($id) {
+        $this->load->model('Stock_model', 'stock');
+        $data = $this->input->post();
+        $this->stock->update_stock_out($id, $data);
+        echo json_encode(['status' => 'success']);
+    }
+
+    public function delete_stock_out($id) {
+        $this->load->model('Stock_model', 'stock');
+        $this->stock->delete_stock_out($id);
+        echo json_encode(['status' => 'success']);
+    }
 } 
