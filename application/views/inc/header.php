b50d2face30da685b284e19c425ad239ec7d4bec
Umakant Yadav <umakant171991@gmail.com>
2025-06-12 09:32:16 +0530

    logo

--- application/views/inc/header.php
+++ application/views/inc/header.php
@@ -114,9 +114,9 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="<?=base_url()?>" class="brand-link">
-      <img src="<?= base_url();?>assets/dist/img/new_logo.jpg" alt="Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <!-- <span class="brand-text font-weight-light">DOUBLE</span> -->
+    <a href="<?=base_url() ?>" class="brand-link">
+      <img src="<?= base_url();?>assets/dist/img/desktop_logo.jpg" alt="Desktop Logo" class="brand-image img-circle elevation-3 d-none d-md-inline" style="opacity: .8">
+      <img src="<?= base_url();?>assets/dist/img/mobile_logo.jpg" alt="Mobile Logo" class="brand-image img-circle elevation-3 d-inline d-md-none" style="opacity: .8">
       <strong><?= $this->session->userdata('name'); ?></strong> 
       (<small>
         <?php
