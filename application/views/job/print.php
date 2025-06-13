185c6f3d56de4e76cf645cfb183b139b4d813ffd
Umakant Yadav <umakant171991@gmail.com>
2025-06-13 11:30:27 +0530

    Update print.php

--- application/views/job/print.php
+++ application/views/job/print.php
@@ -47,7 +47,7 @@ if (isset($all_branches) && isset($jobs[0]->branch)) {
       <table class="pdf-header-table" style="margin-top:20px;">
         <tr>
           <td class="logo-cell">
-            <img src="<?= base_url('assets/dist/img/desktop_logo.jpg') ?>" style="height:70px;">
+            <img src="<?= base_url('assets/dist/img/new_logo.jpg') ?>" style="height:70px;">
           </td>
           <td class="center-cell">
             <div style="font-size:18px;font-weight:bold;">Cover Me Ashburton Limited</div>
