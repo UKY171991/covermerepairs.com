922a86d4149589b37a8a51069bbf7832ec46ec45
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-06-16 11:26:51 +0530

    Update print.php

--- application/views/job/print.php
+++ application/views/job/print.php
@@ -152,17 +152,10 @@ if (isset($all_branches) && isset($jobs[0]->branch)) {
         <div style="font-weight:bold;">Inspection Fee</div>
         $50 inc GST inspection fee will be deducted from final repair cost. <input type="checkbox" class="checkbox" <?php echo (isset($jobs[0]->inspection_fee_paid) && $jobs[0]->inspection_fee_paid == 1) ? 'checked' : ''; ?> disabled> Inspection Paid
                   </div>
-      <div class="static-box">
-        <div style="font-weight:bold;">Loan Device</div>
-        The customer must return the loan device in good condition after the repair. Failing to comply will forfeit the deposit fee.<br>
-        <?php if (isset($jobs[0]->loan_device_details) && trim((string)$jobs[0]->loan_device_details) !== ''): ?>
-          <strong>Details Provided:</strong>
-          <div style="padding-left: 10px; border: 1px dashed #eee; margin-top: 5px; margin-bottom: 5px; background: #f9f9f9; padding: 5px;">
-            <?= nl2br(htmlspecialchars(trim((string)$jobs[0]->loan_device_details))) ?>
-          </div>
-        <?php endif; ?>
-        [Make: ] [Model: ] [IMEI: ] [Deposit Taken: ] [Accessories: <span class="checkbox"></span> Battery <span class="checkbox"></span> Charger / Other :]
-                  </div>
+      <div class="col-md-12">
+        <p><strong>Loan Device</strong></p>
+        <p>The customer must return the loan device in good condition after the repair. Failing to comply will forfeit the deposit fee.</p>
+      </div>
       <div class="static-box">
         <div style="font-weight:bold;">Cover Me Device Drop Off/ Pickup Conditions</div>
         <ul class="bullet-list">
